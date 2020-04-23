5dd12ae690a4ca9e7f69076b79077b64fb9d8419
OldMoM <1090347251@qq.com>
2020-04-23 14:29:41 +0800

    准备阶段大致完成

---
+++ Assets/UIScript/PrepareStateEvent.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Peixi
+{
+    public class PrepareStateEvent : MonoBehaviour
+    {
+        public PlayableDirector director;
+        public PlayableAsset[] timeLines;
+        public GameObject inquireFrame;
+
+        delegate void EmptyEngine();
+        EmptyEngine engine;
+
+        public event Action onRoundStart;
+        public event Action onRoundEnd;
+        // Start is called before the first frame update
+        void Start()
+        {
+            director = GetComponent<PlayableDirector>();
+
+            onRoundEnd += OnPrepareStateEnd;
+            onRoundStart += OnPrepareStateStart;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (engine != null)
+            {
+                engine();
+            }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                OnTimeOut();
+            }
+        }
+
+        public void OnBribeButtonPressed()
+        {
+            print("点击了贿赂按钮");
+            if (director.state == PlayState.Paused)
+            {
+                director.playableAsset = timeLines[1];
+                director.Play();
+            }
+        }
+
+        public void OnDealPlayer1ButtonPressed()
+        {
+            inquireFrame.SetActive(true);
+        }
+
+        /// <summary>
+        /// 接收开始准备阶段的消息
+        /// </summary>
+        public void OnPrepareStateStart()
+        {
+
+        }
+        /// <summary>
+        /// 发出结束准备阶段的消息
+        /// </summary>
+        public void OnPrepareStateEnd()
+        {
+
+        }
+
+        public void OnConfirmBribeButtonPressed()
+        {
+
+        }
+
+        public void OnCancelBribeButtonPressed()
+        {
+
+        }
+
+        public void OnBribeMessageReceived()
+        {
+
+        }
+
+        public void OnRollCardButtonPressed()
+        {
+
+        }
+
+        public void OnConfirmRollCardButtonPressed()
+        {
+
+        }
+
+        public void OnCancelRollCardButtonPressed()
+        {
+
+        }
+        /// <summary>
+        /// 时间到或者按下结束按钮
+        /// </summary>
+        public void OnTimeOut()
+        {
+            //play round end aniamtion
+            director.playableAsset = timeLines[0];
+            director.time = director.duration;
+            engine += PlayStateEndAnim;
+        }
+
+        void PlayStateEndAnim()
+        {
+            director.time -= Time.deltaTime;
+            director.Evaluate();
+            if (director.time <= 0)
+            {
+                director.time = 0;
+                director.Pause();
+                engine -= PlayStateEndAnim;
+            }
+        }
+    }
+}
---
+++ Assets/UIScript/Timer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Playables;
+namespace Peixi
+{
+    public class Timer : MonoBehaviour
+    {
+        Text timerNumber;
+        Button button;
+        Animation anim;
+        public int duration = 60;
+        int time;
+        public PlayableDirector directer;
+        // Start is called before the first frame update
+        void Start()
+        {
+            timerNumber = GetComponentInChildren<Text>();
+            button = GetComponent<Button>();
+            anim = GetComponent<Animation>();
+
+            ResetTimer();
+            PrepareStateEvent prepare = FindObjectOfType<PrepareStateEvent>();
+            prepare.onRoundStart += ResetTimer;
+        }
+        void ResetTimer()
+        {
+            time = duration;
+            button.interactable = true;
+            StartCoroutine(TimeCountDown());
+        }
+        public void OnbuttonPressed()
+        {
+            print("click timer button");
+            anim.Play();
+            button.interactable = false;
+            timerNumber.text = "00";
+            FindObjectOfType<PrepareStateEvent>().OnTimeOut();
+        }
+        IEnumerator TimeCountDown()
+        {
+            timerNumber.text = time.ToString();
+            yield return new WaitForSeconds(1);
+            time -= 1;
+            if (time <= 0)
+            {
+                OnbuttonPressed();
+            }
+            else
+            {
+                StartCoroutine(TimeCountDown());
+            }
+        }
+    }
+}
